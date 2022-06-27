d4292fd81fafafaf87221926f86745e6550ddf6d
dres0 <humberto.ferreiray@gmail.com>
2022-06-27 04:15:29 -0400

    Categories added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_21_014300) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_27_081436) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.string "description"
