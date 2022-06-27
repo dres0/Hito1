30675449cd8768ce067b506b794200ce86b01c64
dres0 <humberto.ferreiray@gmail.com>
2022-06-27 05:23:10 -0400

    user admin ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   devise_for :admins
   get 'home/index'
-  resources :categories
-  resources :products
-  root 'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'pages#index'
+	authenticate :admin do
+		resources :products
+		resources :categories
+	end
 end
